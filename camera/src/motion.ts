f371344504aa8a296d7d4bced97fac5419747c64
Sergei <sergei@sergei-hp>
2019-02-05 21:37:49 -0600

    WIP on motion detection.

--- camera/src/motion.ts
+++ camera/src/motion.ts
@@ -4,9 +4,92 @@ const video = new cv.VideoCapture(0);
 video.set(cv.CAP_PROP_FRAME_WIDTH, 1280);
 video.set(cv.CAP_PROP_FRAME_HEIGHT, 768);
 
-setInterval(() => {
-	console.log('Get frame');
+const bgSubtractor = new cv.BackgroundSubtractorMOG2();
+
+
+while(1) {
 	var image = video.read();
-	if(image) cv.imshow('Video', image);
-	cv.waitKey(20);
-}, 100);
+	if(!image) continue;
+
+	const foreGroundMask = bgSubtractor.apply(image);
+
+	const iterations = 2;
+	const dilated = foreGroundMask.dilate(
+		cv.getStructuringElement(cv.MORPH_ELLIPSE, new cv.Size(4, 4)),
+		new cv.Point2(-1, -1),
+		iterations
+	);
+	const blurred = dilated.blur(new cv.Size(10, 10));
+	const thresholded = blurred.threshold(200, 255, cv.THRESH_BINARY);
+	console.log('motion: %j', thresholded.countNonZero());
+
+	const minPxSize = 4000;
+	drawRectAroundBlobs(thresholded, image, minPxSize);
+
+	cv.imshow('image', image);
+	// cv.imshow('delta',  frameDelta);
+	cv.waitKey(100);
+	// lastImage = gray;
+}
+
+function drawRectAroundBlobs(binaryImg, dstImg, minPxSize, fixedRectWidth?) {
+	const {centroids, stats} = binaryImg.connectedComponentsWithStats();
+
+	// pretend label 0 is background
+	for(let label=1; label<centroids.rows; label+=1) {
+		const [x1, y1] = [stats.at(label, cv.CC_STAT_LEFT), stats.at(label, cv.CC_STAT_TOP)];
+		const [x2, y2] = [
+			x1 + (fixedRectWidth || stats.at(label, cv.CC_STAT_WIDTH)),
+			y1 + (fixedRectWidth || stats.at(label, cv.CC_STAT_HEIGHT))
+		];
+		const size = stats.at(label, cv.CC_STAT_AREA);
+		const red = new cv.Vec3(0,0,255);
+		if(minPxSize<size) {
+			dstImg.drawRectangle(new cv.Point2(x1,y1), new cv.Point2(x2,y2), {color:red, thickness:2});
+		}
+	}
+}
+
+/*
+var lastImage = null;
+var count = 0;
+var guassSize = 5;
+var lowThresh = 20;
+var highThresh = 255;
+
+while(1) {
+	
+	var image = video.read();
+	if(!image) continue;
+
+	var gray = image.bgrToGray();
+
+	if(!lastImage) {
+		lastImage = gray;
+		continue;
+	}
+	gray = gray.gaussianBlur(new cv.Size(guassSize,guassSize), 5,5);
+	
+	var frameDelta:cv.Mat = lastImage.absdiff(gray);
+	var thresh = frameDelta.threshold(lowThresh,highThresh, 0);
+	thresh = thresh.dilate(
+		cv.getStructuringElement(cv.MORPH_ELLIPSE, new cv.Size(4, 4)),
+		new cv.Point(-1, -1),
+		2
+	);
+	var motion = thresh.countNonZero();
+
+	// var contours = thresh.findContours(1,1);
+
+	// console.log('count=%j,\tmotion=%j,\tcontours', count++, motion, contours.length);
+	// for(var i=0;i<contours.length;i++) {
+	// 	image.drawContours(contours, new cv.Vec3(0,0,255), 0);
+	// }
+
+	cv.imshow('image',  image);
+	// cv.imshow('delta',  frameDelta);
+	cv.imshow('thresh', thresh);
+	cv.waitKey(100);
+	// lastImage = gray;
+}
+*/
